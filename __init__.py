03501329a7d3d854a95b0fd61c0a06318ef81485
Guillem Barba <guillem@nan-tic.com>
2014-06-04 11:54:41 +0200

    Correct indent and remove commented code

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
-#This file is part stock_valued module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.pool import Pool
 from .stock import *
 
 
 def register():
     Pool.register(
-#        CompanyScannerInputReports,
-#        CompanyScannerOutputReports,
         Configuration,
         Move,
         ShipmentIn,
