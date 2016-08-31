cd3f56ddec3e74954a3a41f3280a7243ecaea856
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-31 13:24:01 +0200

    Update test to version 4.0

--- tests/test_stock_scanner.py
+++ tests/test_stock_scanner.py
@@ -4,29 +4,16 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class StockScannerTestCase(unittest.TestCase):
+class StockScannerTestCase(ModuleTestCase):
     '''
     Test Stock Scanner module.
     '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_scanner')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('stock_scanner')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+    module = 'stock_scanner'
 
 
 def suite():
@@ -35,5 +22,6 @@ def suite():
             StockScannerTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_stock_shipment_out.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
