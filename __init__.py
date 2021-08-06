7bc01af1f630310998712f861b6e2630ea02a17e
juanjo-nan <57089222+juanjo-nan@users.noreply.github.com>
2021-08-06 10:50:35 +0200

    Move POC picking  to stock_scanner module. (#1)
    Fix show last product.
    Now we order the pending moves by locations.
    If there are diferents locations, we show the name before the move.
    Fix picking more than one unit at the same time.
    
    046607

--- __init__.py
+++ __init__.py
@@ -1,14 +1,21 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .stock import *
+from . import stock
+from . import picking
 
 
 def register():
     Pool.register(
-        Configuration,
-        Move,
-        ShipmentIn,
-        ShipmentOut,
-        ShipmentOutReturn,
+        stock.Configuration,
+        stock.Move,
+        stock.ShipmentIn,
+        stock.ShipmentOut,
+        stock.ShipmentOutReturn,
+        picking.StockPickingShipmentOutAsk,
+        picking.StockPickingShipmentOutScan,
+        picking.StockPickingShipmentOutResult,
         module='stock_scanner', type_='model')
+    Pool.register(
+        picking.StockPickingShipmentOut,
+        module='stock_scanner', type_='wizard')
