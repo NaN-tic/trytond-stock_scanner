197480a253ad611c9bd077271afb8a1e576780c2
Raimon Esteve <raimonesteve@gmail.com>
2022-03-30 13:12:41 +0200

    complete picking

--- inventory.py
+++ inventory.py
@@ -6,6 +6,7 @@ from trytond.pool import Pool
 from trytond.exceptions import UserError
 from trytond.i18n import gettext
 from trytond.pyson import Bool, Eval
+from trytond.transaction import Transaction
 
 
 class StockScannerInventoryAsk(ModelView):
@@ -44,6 +45,7 @@ class StockScannerInventoryAsk(ModelView):
                 (Eval('to_inventory') != 'complete') | Bool(Eval('inventory'))),
             'required': Eval('to_inventory') == 'complete'
         }, depends=['to_inventory', 'inventory'])
+    load_complete_lines = fields.Boolean("Load Complete Lines", readonly=True)
 
     @staticmethod
     def default_to_inventory():
@@ -52,6 +54,22 @@ class StockScannerInventoryAsk(ModelView):
     @staticmethod
     def default_empty_quantity():
         return 'empty'
+
+    @staticmethod
+    def default_load_complete_lines():
+        return False
+
+    @staticmethod
+    def default_stop_load_complete_lines():
+        return False
+
+    @fields.depends('inventory')
+    def on_change_inventory(self):
+        self.load_complete_lines = True if self.inventory else False
+
+    @fields.depends('to_inventory')
+    def on_change_to_inventory(self):
+        self.load_complete_lines = True if self.to_inventory == 'complete' else False
 
 
 class StockScannerInventoryScan(ModelView):
@@ -69,6 +87,9 @@ class StockScannerInventoryScan(ModelView):
     product = fields.Many2One('product.product', "Product", readonly=True)
     to_pick = fields.Char("To pick")
     lines = fields.Text("Lines", readonly=True)
+    complete_lines = fields.One2Many('stock.inventory.line', 'inventory',
+        "Complete Lines", readonly=True)
+    stop_complete_lines = fields.Boolean("Stop Complete Lines", readonly=True)
 
 
 class StockScannerInventoryResult(ModelView):
@@ -88,10 +109,10 @@ class StockScannerInventory(Wizard):
             ])
     scan = StateView('stock.scanner.inventory.scan',
         'stock_scanner.stock_scanner_inventory_scan', [
-            Button('Cancel', 'end', 'tryton-cancel'),
-            Button('Back', 'ask', 'tryton-back'),
+            Button('', 'end', 'tryton-cancel'),
+            Button('', 'ask', 'tryton-back'),
             Button('Pick', 'pick', 'tryton-launch', True),
-            Button('Done', 'done', 'tryton-ok'),
+            Button('', 'done', 'tryton-ok'),
             ])
     pick = StateTransition()
     done = StateTransition()
@@ -122,6 +143,14 @@ class StockScannerInventory(Wizard):
                 if line.product == self.scan.product:
                     line.quantity = quantity
                     line.save()
+                    # remove product from complete_lines
+                    if self.scan.complete_lines:
+                        lines = list(self.scan.complete_lines)
+                        if line in lines:
+                            lines.remove(line)
+                        if not lines:
+                            self.scan.stop_complete_lines = True
+                        self.scan.complete_lines = lines
         else:
             products = Product.search([
                 ('rec_name', '=', to_pick),
@@ -134,7 +163,7 @@ class StockScannerInventory(Wizard):
                     line = InventoryLine()
                     line.inventory = self.scan.inventory
                     line.product = product
-                    line.quantity = 1 if config.scanner_inventory_quantity else 0
+                    line.quantity = 0
                     line.on_change_product()
                     line.save()
                 self.scan.product = product
@@ -160,6 +189,7 @@ class StockScannerInventory(Wizard):
         self.scan.product = None
         self.scan.to_pick = None
         self.scan.lines = None
+        self.scan.stop_complete_lines = False
         return {}
 
     def default_scan(self, fields):
@@ -168,6 +198,7 @@ class StockScannerInventory(Wizard):
         InventoryLine = pool.get('stock.inventory.line')
         Date = pool.get('ir.date')
         Configuration = pool.get('stock.configuration')
+        Product = pool.get('product.product')
 
         config = Configuration(1)
 
@@ -196,11 +227,12 @@ class StockScannerInventory(Wizard):
              inventory = self.ask.inventory
         else:
             is_complete = True if self.ask.to_inventory == 'complete' else False
+            inventory_date = Date.today()
 
             inventory = Inventory()
             inventory.location = location
             inventory.lost_found = lost_found
-            inventory.date = Date.today()
+            inventory.date = inventory_date
             if is_complete:
                 inventory.empty_quantity = self.ask.empty_quantity
             inventory.save()
@@ -208,8 +240,31 @@ class StockScannerInventory(Wizard):
                 Inventory.complete_lines([inventory])
                 lines = inventory.lines
                 if lines:
-                    quantity = 1 if config.scanner_inventory_quantity else 0
-                    InventoryLine.write(list(lines), {'quantity': quantity})
+                    if self.ask.empty_quantity == 'empty':
+                        # set quantity to 0
+                        InventoryLine.write(list(lines), {'quantity': 0})
+                    elif self.ask.empty_quantity == 'keep':
+                        # set quantity to expected_quantity
+                        to_write = []
+                        product_ids = [l.product.id for l in inventory.lines]
+                        grouping = Inventory.grouping()
+                        with Transaction().set_context(stock_date_end=inventory_date):
+                            pbl = Product.products_by_location(
+                                [location.id],
+                                grouping=grouping,
+                                grouping_filter=(product_ids,))
+
+                        for line in inventory.lines:
+                            key = (location.id,) + line.unique_key
+                            if key in pbl:
+                                quantity = pbl.pop(key)
+                            else:
+                                quantity = 0.0
+                            values = line.update_values4complete(quantity)
+                            if values:
+                                to_write.extend(([line], values))
+                        if to_write:
+                            InventoryLine.write(*to_write)
 
         defaults['inventory'] = inventory.id
 
@@ -217,10 +272,26 @@ class StockScannerInventory(Wizard):
             defaults['product'] = self.scan.product and self.scan.product.id
 
         if hasattr(self.scan, 'lines'):
+            if self.scan.stop_complete_lines:
+                lines = []
+            elif hasattr(self.scan, 'complete_lines') and self.scan.complete_lines:
+                lines = self.scan.complete_lines
+            else:
+                lines = inventory.lines
+
             defaults['lines'] = [u'<div align="left">'
-                '<font size="4">{} <b>{}</b></font>'
-                '</div>'.format(line.quantity or 0, line.product.rec_name)
-                for line in inventory.lines if ((line.quantity or 0) < line.expected_quantity)]
+                '<font size="4">{} / {}<b>{}</b></font>'
+                '</div>'.format(line.quantity or 0, line.expected_quantity, line.product.rec_name)
+                for line in lines]
+
+            # complete inventory do control products that are picked and not show
+            if self.ask.load_complete_lines:
+                defaults['complete_lines'] = [l.id for l in inventory.lines]
+                self.ask.load_complete_lines = False
+            elif hasattr(self.scan, 'complete_lines'):
+                defaults['complete_lines'] = [l.id for l in self.scan.complete_lines]
+                self.ask.load_complete_lines = False
+
         return defaults
 
     def default_result(self, fields):
