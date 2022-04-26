efac2560263535eaf0ae015f13a4e5ef30525634
Raimon Esteve <raimonesteve@gmail.com>
2022-04-26 12:13:34 +0200

    Scanner inventory
    #047334

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from . import inventory
 from . import stock
 from . import picking
 
-
 def register():
     Pool.register(
+        inventory.StockScannerInventoryAsk,
+        inventory.StockScannerInventoryScan,
+        inventory.StockScannerInventoryResult,
         stock.Configuration,
         stock.Move,
         stock.ShipmentIn,
@@ -17,5 +20,6 @@ def register():
         picking.StockPickingShipmentOutResult,
         module='stock_scanner', type_='model')
     Pool.register(
+        inventory.StockScannerInventory,
         picking.StockPickingShipmentOut,
         module='stock_scanner', type_='wizard')
